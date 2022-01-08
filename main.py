5f08a133ae97d0444f72e4a9100b8c948ec1c187
efarthou@gmail.com <El23291*>
2022-01-08 17:37:30 -0300

    Caixa de texto para responder aos questionamentos de tratamento

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import os
 import sys
 import sensiveis as conf
 
+
+tabela = ''
+tabelafornecedor = ''
+
+
 pergunta = messagebox.msgbox('Tem colunas de pré-cabeçalho?', messagebox.MB_YESNO,
                              'Tratamento de cabeçalho')
 if pergunta == messagebox.IDYES:
     quantcolunas = int(messagebox.criarinputbox('Quant. colunas do arquivo', 'Quantas colunas tem o arquivo '
-                                                                         '(lembrar que o SAP adiciona 2 colunas a '
-                                                                         'mais)?'))
+                                                                             '(lembrar que o SAP adiciona 2 colunas a '
+                                                                             'mais)?'))
     if int(quantcolunas) < 4 or not str(quantcolunas).isnumeric():
         messagebox.msgbox('Valor inválido', messagebox.MB_OK, 'Valor inválido informado!')
         sys.exit()
@@ -24,7 +29,7 @@ if len(arquivo_caminho_origem) == 0:
                       'Caminho dos Arquivos a Tratar não selecionado')
     sys.exit()
 
-resultado = ''
+resultado = 0
 
 arquivo_caminho_destino = aux.caminhoselecionado(3)
 if len(arquivo_caminho_destino) == 0:
@@ -48,7 +53,9 @@ for arquivo in aux.retornaarquivos(arquivo_caminho_origem):
     caminholog = caminhosalvo.upper()
     caminholog = caminholog.replace('.TXT', '.LOG')
     tempopergunta = time.time()
-    tabela = 'GIG OPEX Entrada Compromisso Ex Carga'
+    # [GIG Analise Compromisso]
+
+    # tabela = 'GIG OPEX Entrada Compromisso Ex Carga'
     if len(caminhofornec) > 0:
         caminhoerrofornec = caminhofornec.upper()
         caminhoerrofornec = caminhoerrofornec.replace('.TXT', '.ERR')
@@ -56,18 +63,33 @@ for arquivo in aux.retornaarquivos(arquivo_caminho_origem):
         caminhologfornec = caminhologfornec.replace('.TXT', '.LOG')
         tabelafornecedor = 'GIG IndicexFornecedor'
     tempopergunta = time.time()
-    if resultado == '':
+    if resultado == 0:
         resultado = messagebox.msgbox('Subir pro banco?', messagebox.MB_YESNO, 'Carga SQL')
     temporesposta = time.time()
     if resultado == messagebox.IDYES:
-        os.system(
-            'bcp ' + conf.schema + '."[' + tabela + ']" IN "' + caminhosalvo + '" -t "|" -C '
-                                                                               'SQL_Latin1_General_CP1_CI_AS -c -S '
-            + conf.endbanco + ' -U ' + conf.usrbanco + ' -P ' + conf.pwdbanco + ' -d ' + conf.nomebanco + ' -e "' +
-            caminhoerro + '" -F 2 > "' + caminholog + '"')
-        if len(caminhofornec) > 0:
-            os.system(
-                'bcp ' + conf.schema + '."[' + tabelafornecedor + ']" IN ' + caminhofornec + ' -t "|" -C SQL_Latin1_General_CP1_CI_AS -c -S ' + conf.endbanco + ' -U ' + conf.usrbanco + ' -P ' + conf.pwdbanco + ' -d ' + conf.nomebanco + ' -e ' + caminhoerrofornec + ' -F 2 > ' + caminhologfornec)
+        if len(tabela) == 0:
+            tabela = messagebox.criarinputbox('Tabela Banco', 'Digite a tabela a ser carregada:')
+
+        if len(tabela) > 0:
+            # Posterior criação de verificação de quantidade de campos e se a tabela existe
+            # (posso criar uma verificação de tipos dos campos também)
+            if len(caminhofornec) > 0:
+                tabelafornecedor = messagebox.criarinputbox('Tabela Banco',
+                                                            'Digite a tabela de fornecedor a ser carregada:')
+
+            if len(tabela) > 0 and ((len(caminhofornec) > 0 and len(tabelafornecedor) > 0) or len(caminhofornec) == 0):
+                os.system('bcp ' + conf.schema + '."[' + tabela + ']" IN "'
+                          + caminhosalvo + '" -t "|" -C SQL_Latin1_General_CP1_CI_AS -c -S ' +
+                          conf.endbanco + ' -U ' + conf.usrbanco + ' -P ' + conf.pwdbanco + ' -d '
+                          + conf.nomebanco + ' -e "' + caminhoerro + '" -F 2 > "' + caminholog + '"')
+                if len(caminhofornec) > 0:
+                    os.system('bcp ' + conf.schema + '."[' + tabelafornecedor + ']" IN ' + caminhofornec +
+                              ' -t "|" -C SQL_Latin1_General_CP1_CI_AS -c -S ' + conf.endbanco + ' -U ' +
+                              conf.usrbanco + ' -P ' + conf.pwdbanco + ' -d ' + conf.nomebanco + ' -e ' +
+                              caminhoerrofornec + ' -F 2 > ' + caminhologfornec)
+        else:
+            messagebox.msgbox('Valor inválido', messagebox.MB_OK, 'Nome da tabela não informado!')
+            sys.exit()
 
 tempofim = time.time()
 
